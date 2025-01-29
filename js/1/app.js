78dee4a7772d0eb600870438081a1ea28bb02a44
Adelina Kalykova <adelinakalyk@gmail.com>
2025-01-29 15:36:04 +0600

    add basic structure and js-tasks

---
+++ js/1/app.js
@@ -0,0 +1,26 @@
+// let word1 = "Adelina";
+// let word2 = "Ayturgan";
+// if (word1[0] === word2[0]) {
+//   console.log("Буквы совпадают");
+// } else {
+//   console.log("Буквы не совпадают");
+// }
+
+// let number = 95904598;
+// let str = number.toString();
+// console.log(str[0]);
+
+// let number = 95904598;
+// let str = number.toString();
+// console.log(str[str.length -1]);
+
+// let number = 95904598;
+// let str = number.toString();
+// let Sum = +str[0] + +str[str.length - 1];
+// console.log(Sum);
+
+
+// let number = 95904598;
+// let str = number.toString();
+// console.log(str.length)
+
